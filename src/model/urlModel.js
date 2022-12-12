6072f490dd5049cde64482a4a74510ac68116df8
manuu07 <manusrivastava229@gmail.com>
2022-12-12 16:53:15 +0530

    model

--- src/model/urlModel.js
+++ src/model/urlModel.js
@@ -1,1 +1,26 @@
 const mongoose=require('mongoose')
+
+//{ urlCode: { mandatory, unique, lowercase, trim }, longUrl: {mandatory, valid url}, shortUrl: {mandatory, unique} }
+
+const urlSchema=new mongoose.Schema({
+    longUrl:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    shortUrl:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    urlCode:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    }
+})
+
+module.exports=mongoose.model('Url',urlSchema)
