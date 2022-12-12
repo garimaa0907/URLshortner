4dc2a6eba03f6460dd277543604c67f9c85ac06d
manuu07 <manusrivastava229@gmail.com>
2022-12-12 16:26:59 +0530

    project started

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const app=express()
+const route=require('./routes/route')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect('mongodb+srv://UrlShortner:UrlShortner@cluster0.nj2ofna.mongodb.net/group2Database',
+ {useNewUrlParser:true})
+ .then(()=>console.log("MongoDb is connected"))
+ .catch(err=>console.log(err.message))
+
+ app.use('/',route)
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express is running on port " + (process.env.PORT || 3000))
+})
---
+++ src/routes/route.js
@@ -0,0 +1,5 @@
+const express=require('express')
+const router=express.Router()
+const urlController=require('../controller/urlController')
+
+module.exports=router
